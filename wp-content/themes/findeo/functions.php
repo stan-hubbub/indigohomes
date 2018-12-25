c1f0949a0debcc8f9639b86fe51d0e5722ccc637
Stan <sfamilleza@gmail.com>
2018-12-25 22:15:36 +0800

    remove billing details

--- wp-content/themes/findeo/functions.php
+++ wp-content/themes/findeo/functions.php
@@ -303,3 +303,25 @@ function custom_map_link($address, $mmm ){
 	return '<a class="listing-address" href="' . get_the_permalink($post) .'#location"><i class="fa fa-map-marker"></i>' . esc_html( strip_tags( $friendly_address ) ) . '</a>';
 }
 add_filter('the_property_map_link','custom_map_link',10,2);
+
+// hide billing details on checkout
+add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+function custom_override_checkout_fields( $fields ) {
+    unset($fields['billing']['billing_first_name']);
+    unset($fields['billing']['billing_last_name']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_address_1']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_city']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_country']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['billing']['billing_phone']);
+    unset($fields['order']['order_comments']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_email']);
+    unset($fields['billing']['billing_city']);
+    return $fields;
+}
