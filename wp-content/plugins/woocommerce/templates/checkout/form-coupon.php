08be58758cc9b24d13f3c70ea6ec134b803ecb09
Stan <sfamilleza@gmail.com>
2018-12-29 00:34:29 +0800

    coupon style

--- wp-content/plugins/woocommerce/templates/checkout/form-coupon.php
+++ wp-content/plugins/woocommerce/templates/checkout/form-coupon.php
@@ -23,7 +23,7 @@ if ( ! wc_coupons_enabled() ) { // @codingStandardsIgnoreLine.
 
 ?>
 <div class="woocommerce-form-coupon-toggle">
-	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Click here to enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
+	<?php wc_print_notice( apply_filters( 'woocommerce_checkout_coupon_message', __( 'Have a coupon?', 'woocommerce' ) . ' <a href="#" class="showcoupon">' . __( 'Enter your code', 'woocommerce' ) . '</a>' ), 'notice' ); ?>
 </div>
 
 <form class="checkout_coupon woocommerce-form-coupon form-inline" method="post" style="display:none">
