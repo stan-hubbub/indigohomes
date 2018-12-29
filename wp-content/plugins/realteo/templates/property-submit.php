bdabf2bff1ca7560acdd92dc160f61f811345c5c
Stan <sfamilleza@gmail.com>
2018-12-29 22:13:44 +0800

    Update property-submit.php

--- wp-content/plugins/realteo/templates/property-submit.php
+++ wp-content/plugins/realteo/templates/property-submit.php
@@ -93,8 +93,8 @@ endif;
 		<input type="hidden" 	name="property_id" value="<?php echo esc_attr( $data->property_id ); ?>" />
 		<input type="hidden" 	name="step" value="<?php echo esc_attr( $data->step ); ?>" />
 		<div class="input-with-icon margin-top-25 margin-bottom-30 big">
-			<i class="fa fa-arrow-circle-right" style="margin-top: 1px;"></i>
 			<input type="submit" name="submit_property" class="button" value="<?php echo esc_attr( $data->submit_button_text ); ?>" />
+			<i class="fa fa-arrow-circle-right" style="margin-top: 1px;"></i>
 		</div>
 	</p>
 	
