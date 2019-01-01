ea9567d1a51d34ebdecf1fec29ba8b0f3250334a
SAF <stanleyfamilleza@gmail.com>
2019-01-01 11:26:22 +0800

    register-ui (#21)
    * Update login.php
    
    * Update login.php

--- wp-content/plugins/realteo/templates/account/login.php
+++ wp-content/plugins/realteo/templates/account/login.php
@@ -88,7 +88,7 @@ if ( isset( $_REQUEST['register-errors'] ) ) {
 				        $password_field = realteo_get_option('realteo_generate_password');
 				        if($password_field) {
 							printf(
-				                __( 'You have successfully registered to <strong>%s</strong>.', 'realteo' ),
+				                __( 'You have successfully registered to <strong>%s</strong>. You may now login.', 'realteo' ),
 				                get_bloginfo( 'name' )
 				            );
 				        } else {
@@ -175,24 +175,24 @@ if ( isset( $_REQUEST['register-errors'] ) ) {
 				<?php } ?>
 			        <p class="form-row">
 			            <label for="email"><?php esc_html_e( 'Email', 'realteo' ); ?> <strong>*</strong></label>
-			            <input type="text" name="email" id="email">
+			            <input type="text" name="email" id="email" required="required">
 			        </p>
 			 		
 			 		<?php if(realteo_get_option('realteo_generate_password')) : ?>
 			        <p class="form-row">
-			            <label for="password"><?php esc_html_e( 'Password', 'realteo' ); ?></label>
-			            <input type="password" name="password" id="password">
+			            <label for="password"><?php esc_html_e( 'Password', 'realteo' ); ?> <strong>*</strong></label>
+			            <input type="password" name="password" id="password" required="required">
 			        </p>
 			    	<?php endif; ?>
 
 			        <p class="form-row">
-			            <label for="first_name"><?php esc_html_e( 'First name', 'realteo' ); ?></label>
-			            <input type="text" name="first_name" id="first-name">
+			            <label for="first_name"><?php esc_html_e( 'First name', 'realteo' ); ?> <strong>*</strong></label>
+			            <input type="text" name="first_name" id="first-name" required="required">
 			        </p>
 			 
 			        <p class="form-row">
-			            <label for="last_name"><?php esc_html_e( 'Last name', 'realteo' ); ?></label>
-			            <input type="text" name="last_name" id="last-name">
+			            <label for="last_name"><?php esc_html_e( 'Last name', 'realteo' ); ?> <strong>*</strong></label>
+			            <input type="text" name="last_name" id="last-name" required="required">
 			        </p>
 
 			        <?php do_action('realteo_registration_form') ?>
@@ -216,9 +216,9 @@ if ( isset( $_REQUEST['register-errors'] ) ) {
 							?>
 				 		</p>
 			 		<?php } ?>
-			        <p class="form-row margin-top-30 margin-bottom-30">
+			        <!-- <p class="form-row margin-top-30 margin-bottom-30">
 			            <?php esc_html_e( 'Note: Your password will be generated automatically and sent to your email address.', 'realteo' ); ?>
-			        </p>
+			        </p> -->
 					<?php $recaptcha_status = realteo_get_option('realteo_recaptcha');
 	            	if($recaptcha_status) { ?>
 			        <p class="form-row captcha_wrapper">
