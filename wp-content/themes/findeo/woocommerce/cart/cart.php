9899a72ec36bc08a7ddf3b9849200771c99587d2
Stan <sfamilleza@gmail.com>
2019-01-04 05:09:02 +0800

    Update cart.php

--- wp-content/themes/findeo/woocommerce/cart/cart.php
+++ wp-content/themes/findeo/woocommerce/cart/cart.php
@@ -47,10 +47,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 
 						<td class="product-name" data-title="<?php esc_attr_e( 'Product','findeo' ); ?>">
 						<?php
-						if ( ! $product_permalink ) {
 							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-						} else {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', sprintf( '<a href="%s">%s</a>', esc_url( $product_permalink ), $_product->get_name() ), $cart_item, $cart_item_key ) );
 						}
 
 						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
