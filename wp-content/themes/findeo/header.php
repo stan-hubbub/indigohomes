d32df0566e7ef554d6adc31bb424396139f9776e
Stan <sfamilleza@gmail.com>
2019-01-04 03:04:46 +0800

    Update header.php

--- wp-content/themes/findeo/header.php
+++ wp-content/themes/findeo/header.php
@@ -16,7 +16,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-<link rel="stylesheet" media="screen" href="<?php echo get_stylesheet_directory_uri(); ?>/css/findeo-custom.css?v=0.2.3">
+<link rel="stylesheet" media="screen" href="<?php echo get_stylesheet_directory_uri(); ?>/css/findeo-custom.css?v=0.2.4">
 <?php wp_head(); ?>
 </head>
 
