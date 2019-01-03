5ece6954393af4520db66e786cbe9ce9845c7aa9
Stan <sfamilleza@gmail.com>
2019-01-04 05:14:16 +0800

    Update cart.php

--- wp-content/themes/findeo/woocommerce/cart/cart.php
+++ wp-content/themes/findeo/woocommerce/cart/cart.php
@@ -46,19 +46,20 @@ do_action( 'woocommerce_before_cart' ); ?>
 					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
 						<td class="product-name" data-title="<?php esc_attr_e( 'Product','findeo' ); ?>">
-						<?php
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' );
-						
-						do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
+						<h4>
+							<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key )); ?>
+						</h4>	
+						<?php 
+							do_action( 'woocommerce_after_cart_item_name', $cart_item, $cart_item_key );
 
-						// Meta data.
-						echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
+							// Meta data.
+							echo wc_get_formatted_cart_item_data( $cart_item ); // PHPCS: XSS ok.
 
-						// Backorder notification.
-						if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
-							echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'findeo' ) . '</p>', $product_id ) );
-						}
-						?>
+							// Backorder notification.
+							if ( $_product->backorders_require_notification() && $_product->is_on_backorder( $cart_item['quantity'] ) ) {
+								echo wp_kses_post( apply_filters( 'woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__( 'Available on backorder', 'findeo' ) . '</p>', $product_id ) );
+							}
+							?>
 						</td>
 
 						<td class="product-price" data-title="<?php esc_attr_e( 'Price','findeo' ); ?>">
