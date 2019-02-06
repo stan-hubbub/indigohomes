d6e1741d08431985daeca4f9cc53771f2512f670
Stan <sfamilleza@gmail.com>
2019-02-06 15:07:01 +0800

    new logo
    - favicon
    - default og
    - new logo

--- wp-content/themes/findeo/header.php
+++ wp-content/themes/findeo/header.php
@@ -14,10 +14,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta property="og:image" content="<?php if (function_exists('wp_get_attachment_thumb_url')) {echo wp_get_attachment_thumb_url(get_post_thumbnail_id($post->ID)); }?>" />
+<!-- <meta property="og:image" content="<?php if (function_exists('wp_get_attachment_thumb_url')) {echo wp_get_attachment_thumb_url(get_post_thumbnail_id($post->ID)); }?>" /> -->
+<meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/images/og.png" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-<link rel="stylesheet" media="screen" href="<?php echo get_stylesheet_directory_uri(); ?>/css/findeo-custom.css?v=0.2.40">
+<link rel="stylesheet" media="screen" href="<?php echo get_stylesheet_directory_uri(); ?>/css/findeo-custom.css?v=0.2.41">
 <?php wp_head(); ?>
 </head>
 
