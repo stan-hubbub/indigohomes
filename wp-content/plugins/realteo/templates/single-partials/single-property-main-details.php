6a15e19f33e34f9178ef0374633d6a9c6f4ea132
Stan <sfamilleza@gmail.com>
2019-02-23 18:55:11 +0800

    Update single-property-main-details.php

--- wp-content/plugins/realteo/templates/single-partials/single-property-main-details.php
+++ wp-content/plugins/realteo/templates/single-partials/single-property-main-details.php
@@ -5,7 +5,7 @@ $details_list = Realteo_Meta_Boxes::meta_boxes_main_details();
 $class = (isset($data->class)) ? $data->class : 'property-main-features' ;
 ?>
 <ul class="<?php esc_attr_e($class); ?>">
-	<?php if (!empty(the_property_price)) { ?>
+	<?php if (!empty(the_property_price())) { ?>
 		<li  class="main-detail-price"><?php the_property_price(); ?></li>
 	<?php } ?>
 <?php
